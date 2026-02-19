b880a390bab90366783c5fef62d70ab80093a211
chamichu1920-tech <chamichu1920@gmail.com>
2026-02-19 13:25:59 -0500

    fase video 9

---
+++ resources/views/admin/usuarios/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+@section('content')
+<div class="row">
+  <h1>Listado de usuarios</h1>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ruta para el admin
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index')->middleware('auth');
+// ruta para el admin - usuarios
+Route::get('/admin/usuarios', [App\Http\Controllers\UsuarioController::class, 'index'])->name('admin.usuarios.index')->middleware('auth');
